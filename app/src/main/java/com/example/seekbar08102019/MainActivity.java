ae6aefe450f0873d72c719db84ca681132d2f7e9
phatdroid94 <phatdroid94@gmail.com>
2019-11-19 21:21:35 +0700

    event onchange sk

--- app/src/main/java/com/example/seekbar08102019/MainActivity.java
+++ app/src/main/java/com/example/seekbar08102019/MainActivity.java
@@ -3,12 +3,37 @@ package com.example.seekbar08102019;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    SeekBar sk;
+    TextView txtProgress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sk = findViewById(R.id.seekbar);
+        txtProgress = findViewById(R.id.textviewProgress);
+
+        sk.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                txtProgress.setText(sk.getProgress() + "");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
